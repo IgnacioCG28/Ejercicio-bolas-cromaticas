3f38ddf0e8d4c85d7a97334c29fe47cf03c478ec
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-11-20 09:56:59 +0100

    update ball

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector(`canvas`);
 const ctx = canvas.getContext(`2d`);
 
-//Definir el ancho y el alto del template
+//Averiguar el ancho y el alto del template
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
@@ -23,4 +23,43 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+
+  // Creación de dibujar (el molde) una bola con métodos propios de 2d en canvas
+  draw() {
+    // Inicio de la ruta/posición para dibujar la bola
+    ctx.beginPath();
+    // Darle un color
+    ctx.fillStyle = this.color;
+    // Forma de la bola
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    //Caso condición choque horizontal (bota), de movimiento positivo + a negativo -
+    if (this.x + this.size >= width) {
+      //Borde derecho
+      this.velX = -Math.abs(this.velX);
+    }
+    if (thix.x - this.size <= 0) {
+      //Borde izquierdo
+      this.velX = Math.abs(this.velX);
+    }
+
+    // Choque vertical
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+  }
+
+
+
+
+
+
+  
 }
